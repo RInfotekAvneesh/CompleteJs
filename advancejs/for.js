f9392bd6db39db639648ee628bd3b11997429055
AvneeshRInfotek <avneeshj832@gmail.com>
2023-12-27 11:56:26 +0530

    update

---
+++ advancejs/for.js
@@ -0,0 +1,29 @@
+// for (let i = 0; i <= 10; i++) {
+//   const element = i;
+//   if (element == 5) {
+//     console.log("5 is best number");
+//   }
+//   console.log(element);
+// }
+
+// for (let i = 0; i <= 4; i++) {
+//   console.log(`outer loop ${i}`);
+//   for (let j = 0; j <= 4; j++) {
+//     console.log(`inner loop ${j} and inner loop value ${i}`);
+//   }
+// }
+
+// print table
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`Outer loop value :${i}`);
+
+//   for (let j = 1; j <= 10; j++) {
+//     console.log(i + "*" + j + "=" + i * j);
+//   }
+// }
+
+let myArr = ["ajay", "rohan", "Ravi", "Akash"];
+for (let i = 0; i < myArr.length; i++) {
+  console.log(myArr[i]);
+}
---
+++ advancejs/while.js
@@ -0,0 +1,30 @@
+// let i = 0;
+// while (i <= 20) {
+//   console.log(`value is:${i}`);
+//   i = i + 2;
+// }
+// let name = ["Avneesh", "Rajesh", "Ravi"];
+// let arr = 0;
+
+// while (arr < name.length) {
+//   console.log(`value is:${name[arr]}`);
+//   arr = arr + 1;
+// }
+
+// do while loop
+
+// let score = 1;
+// do {
+//   console.log(`Score is ${score}`);
+//   score++;
+// } while (score <= 10);
+
+// for of loop
+const num = [2, 3, 4, 5, 5, 67, 7, 8, 9, 9, 56, 777, 8888, 9999];
+for (const arr of num) {
+  console.log(arr);
+}
+const greeting = "Hello JavaScript";
+for (const greet of greeting) {
+  console.log(greet);
+}
