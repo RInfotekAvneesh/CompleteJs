a152d09815267916d8c78a64346a20efd289abd0
AvneeshRInfotek <avneeshj832@gmail.com>
2024-01-07 23:42:47 +0530

    update project

---
+++ advancejs/Project/QuizApp/script.js
@@ -0,0 +1,52 @@
+const questions = [
+  {
+    question:
+      "In the acronym SARAS mela, which are organized frequently, what does R stand for?",
+    answer: [
+      { text: Remote, correct: false },
+      { text: Rural, correct: true },
+      { text: Regressive, correct: false },
+      { text: Right, correct: false },
+    ],
+  },
+  {
+    question:
+      "In the acronym SARAS mela, which are organized frequently, what does R stand for?",
+    answer: [
+      { text: Remote, correct: false },
+      { text: Rural, correct: true },
+      { text: Regressive, correct: false },
+      { text: Right, correct: false },
+    ],
+  },
+  {
+    question:
+      "In the acronym SARAS mela, which are organized frequently, what does R stand for?",
+    answer: [
+      { text: Remote, correct: false },
+      { text: Rural, correct: true },
+      { text: Regressive, correct: false },
+      { text: Right, correct: false },
+    ],
+  },
+  {
+    question:
+      "In the acronym SARAS mela, which are organized frequently, what does R stand for?",
+    answer: [
+      { text: Remote, correct: false },
+      { text: Rural, correct: true },
+      { text: Regressive, correct: false },
+      { text: Right, correct: false },
+    ],
+  },
+  {
+    question:
+      "In the acronym SARAS mela, which are organized frequently, what does R stand for?",
+    answer: [
+      { text: Remote, correct: false },
+      { text: Rural, correct: true },
+      { text: Regressive, correct: false },
+      { text: Right, correct: false },
+    ],
+  },
+];
