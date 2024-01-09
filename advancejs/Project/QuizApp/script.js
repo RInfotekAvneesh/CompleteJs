e1eee515042dd32f4a356e8bedb1f4aab6940d3b
AvneeshRInfotek <avneeshj832@gmail.com>
2024-01-09 10:35:41 +0530

    update project

--- advancejs/Project/QuizApp/script.js
+++ advancejs/Project/QuizApp/script.js
@@ -3,50 +3,81 @@ const questions = [
     question:
       "In the acronym SARAS mela, which are organized frequently, what does R stand for?",
     answer: [
-      { text: Remote, correct: false },
-      { text: Rural, correct: true },
-      { text: Regressive, correct: false },
-      { text: Right, correct: false },
+      { text: "Remote", correct: false },
+      { text: "Rural", correct: true },
+      { text: "Regressive", correct: false },
+      { text: "Right", correct: false },
     ],
   },
   {
-    question:
-      "In the acronym SARAS mela, which are organized frequently, what does R stand for?",
+    question: "Which is largest animal in the world?",
     answer: [
-      { text: Remote, correct: false },
-      { text: Rural, correct: true },
-      { text: Regressive, correct: false },
-      { text: Right, correct: false },
+      { text: "Shark", correct: false },
+      { text: "Blue Whale", correct: true },
+      { text: "Elephant", correct: false },
+      { text: "Giraffe", correct: false },
     ],
   },
   {
-    question:
-      "In the acronym SARAS mela, which are organized frequently, what does R stand for?",
+    question: "Which is the smallest continent in the world?",
     answer: [
-      { text: Remote, correct: false },
-      { text: Rural, correct: true },
-      { text: Regressive, correct: false },
-      { text: Right, correct: false },
+      { text: "Asia", correct: false },
+      { text: "Australia", correct: true },
+      { text: "Arctic", correct: false },
+      { text: "Africa", correct: false },
     ],
   },
   {
-    question:
-      "In the acronym SARAS mela, which are organized frequently, what does R stand for?",
+    question: "Which is the largest desert  in the world?",
     answer: [
-      { text: Remote, correct: false },
-      { text: Rural, correct: true },
-      { text: Regressive, correct: false },
-      { text: Right, correct: false },
-    ],
-  },
-  {
-    question:
-      "In the acronym SARAS mela, which are organized frequently, what does R stand for?",
-    answer: [
-      { text: Remote, correct: false },
-      { text: Rural, correct: true },
-      { text: Regressive, correct: false },
-      { text: Right, correct: false },
+      { text: "Kalahari", correct: false },
+      { text: "Gobi", correct: false },
+      { text: "Sahara", correct: false },
+      { text: "Antarctica", correct: true },
     ],
   },
 ];
+const question = document.getElementById("question");
+const answer_button = document.getElementById("answer-button");
+const next_btn = document.getElementById("next-btn");
+let currentQuestionIndex = 0;
+let score = 0;
+function startQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  next_btn.innerHTML = "Next";
+  showQuestion();
+}
+function showQuestion() {
+  resetState();
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1;
+  question.innerHTML = questionNo + "." + currentQuestion.question;
+  currentQuestion.answer.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answer_button.appendChild(button);
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+
+    button.addEventListener("click", selectAnswer);
+  });
+}
+function resetState() {
+  next_btn.style.display = "none";
+  while (answer_button.firstChild) {
+    answer_button.removeChild(answer_button.firstChild);
+  }
+}
+function selectAnswer(e) {
+  const selectBtn = e.target;
+  const isCorrect = selectBtn.dataset.correct === "true";
+  if (isCorrect) {
+    selectBtn.classList.add("correct");
+  } else {
+    selectBtn.classList.add("incorrect");
+  }
+}
+startQuiz();
